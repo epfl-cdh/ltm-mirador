502c8544812a46ba08edd38a6cbaa859d48c5671
Nicolas Mermoud-Ghraichy <nicolas.mermoud-ghraichy@epfl.ch>
2023-08-21 10:50:59 +0200

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
 });
