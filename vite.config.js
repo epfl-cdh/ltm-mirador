9437f3364e0aaec7820716346f218672e3c0f270
Nicolas Mermoud-Ghraichy <nicolas.mermoud-ghraichy@epfl.ch>
2023-09-12 12:55:16 +0200

    Define base path in Vite configuration so that relative paths are used on build.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
+    base: './',
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
     // global: {},
